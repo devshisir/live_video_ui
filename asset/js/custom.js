149f817082cd65494d35036e513871f7f3ac98c4
devshisir <contact.shisirimran@gmail.com>
2020-11-22 02:17:02 +0600

    add full function all elements

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -2,4 +2,30 @@ $(document).ready(function() {
     var school = $(window);
     var page = $('html, body');
     
+
+    $('.FB_reactions').facebookReactions({
+      //postUrl: "save.php"
+    });
+
+
+    new FgEmojiPicker({
+      trigger: ['#emojipickerButton'],
+      position: ['top', 'left'],
+      emit(obj, triggerElement) {
+        const emoji = obj.emoji;
+        document.querySelector('#maincommentInput').value += emoji;
+      }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
 });
